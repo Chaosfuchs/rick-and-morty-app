4eb7572d6dec2c035e2dfe8ac200751e18988316
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-09 16:37:41 +0200

    Add remove function for id

--- node/test.js
+++ node/test.js
@@ -1,14 +1,15 @@
 import { promises } from 'fs';
-
+import { nanoid } from 'nanoid';
+/*
 promises
   .readFile('database.json', 'utf-8')
   .then(data => JSON.parse(data))
   .then(json => {
-    console.log(json);
+     console.log(json);
   })
   .catch(error_ => {
     throw error_;
-  });
+  }); */
 
 async function getDatabase() {
   const content = await promises.readFile('database.json', 'utf-8');
@@ -20,14 +21,17 @@ async function addUser(user) {
   const data = await getDatabase();
   data.users.push(user);
   await promises.writeFile('database.json', JSON.stringify(data, null, 4));
-  console.log(data);
+  //console.log(data);
 }
 
-addUser({ name: 'Anna', age: 19, hobbies: ['reading'] }); // Aufruf der Add Function
+// addUser({ id: nanoid(), name: 'Anna', age: 20, hobbies: ['reading'] }); // Aufruf der Add Function
 
-async function removeUser(user) {
+async function removeUser(id) {
   const data = await getDatabase();
-  data.users.pop(user);
+  const newArray = data.users.filter(user => user.id !== id);
+  data.users = newArray;
   await promises.writeFile('database.json', JSON.stringify(data, null, 4));
-  console.log(data);
+  console.log(newArray);
 }
+
+removeUser(''); // Aufruf der Remove Function
