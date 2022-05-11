50b1a7a8d6aefb0a46eeedfe9016a24ae897eb42
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-11 15:05:24 +0200

    Add collection container and first data to test

--- components/collectionCard.js
+++ components/collectionCard.js
@@ -1,38 +1,78 @@
 import styled from 'styled-components';
 import useFetch from '../hooks/fetch';
 import StyledLoader from '../components/loader';
-import StyledCard from '../components/characterCard';
 
 export default function CollectionCard() {
-  const { data, loading, error } = useFetch('/api/collection');
+  const { data, loading, error } = useFetch(
+    'https://rickandmortyapi.com/api/character/?page=5'
+  );
 
   return (
-    <>
-      {loading && <StyledLoader />}
-      {error && <div>{error.message}</div>}
+    <div>
       <StyledCollectionContainer>
+        {loading && <StyledLoader />}
+        {error && <div>{error.message}</div>}
         <h3>My collected Cards</h3>
-        <StyledCard />
+        {data?.results.map(character => (
+          <StyledCollectedCard>
+            <img src={character.image}></img>
+            <p>{character.name}</p>
+          </StyledCollectedCard>
+        ))}
       </StyledCollectionContainer>
-    </>
+    </div>
   );
 }
 
 const StyledCollectionContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
   background-color: #f6bd60;
-  font-family: rickAndMortyFonts;
   border-radius: 20px;
   border: 2px solid black;
   box-shadow: 3px 3px 3px grey;
-  margin: 20px;
+  margin: 10px;
+  height: 450px;
 
   h3 {
-    font-size: 40px;
+    text-align: center;
+    font-size: 25px;
     color: #290363;
     text-decoration: underline overline;
+    font-family: rickAndMortyFonts;
+  }
+`;
+
+const StyledCollectedCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid black;
+  margin: 5px;
+  padding: 10px;
+  background-color: #0bf41b;
+  border-radius: 20px;
+  width: 130px;
+  height: 150px;
+  box-shadow: 3px 3px 3px grey;
+
+  img {
+    width: 100px;
+    border-radius: 999px;
+    border: 2px solid green;
+    box-shadow: 3px 3px 3px grey;
+    padding: 0;
+    margin: 0;
+  }
+
+  p {
+    font-size: 20px;
+    text-align: center;
+    padding: 0;
+    margin: 0;
   }
 `;
