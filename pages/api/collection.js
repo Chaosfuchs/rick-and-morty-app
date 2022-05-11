cfab8dd649e0a45c1199e8b3cf40b2661c1ed5ac
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-11 16:57:21 +0200

    Debug problem with writing in mongo db

--- pages/api/collection.js
+++ pages/api/collection.js
@@ -3,11 +3,13 @@ import Character from '../../models/Character';
 import dbConnect from '../../lib/dbConnect';
 
 export default async function handleCollect(req, res) {
+  await dbConnect();
+
   if (req.method === 'POST') {
     try {
       const newItem = await Character.create(
         req.body
-      ); /* create a new Character in the database */
+      ); /* create a new model in the database */
       res.status(201).json({ success: true, data: newItem });
     } catch (error) {
       res.status(400).json({ success: false });
